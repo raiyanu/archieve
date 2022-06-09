54e00cad0d980bbb76497861bcfc4c5cadffb428
RayForeverRK <ray38250968@gmail.com>
2022-06-09 14:37:25 +0530

    Add files via upload

---
+++ cpp/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int i;
+main() {
+	printf("hello world");
+	for (i=0;i==10;i++)
+	{
+		printf("hello world ");
+	}
+	return 0;
+}
