d79cca0f048965d3f14d7e3569c708ebee939ba8
Raiyan Ahmed <ray38250968@gmail.com>
2024-06-23 18:21:59 +0000

    feat(DB): database integrated

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import mongoose from 'mongoose';
-
-mongoose.connect(process.env.MONGO_URI).then(() => {
+import {log as p} from "console";
+await mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log('Connected to MongoDB');
     }).catch((err) => {
     console.log('Error: ', err);
     }
 );
+
+const testDB =  mongoose.Schema({
+    name: String,
+    age: Number
+});
+const testModel = mongoose.model('testModel', testDB);
+try {
+    p(await testModel.findOne());
+    p('Success');
+} catch (error) {
+    p(error);
+}
